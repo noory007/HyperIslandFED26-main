c0d7938551eca065cef67284a468b9197f2b289a
noory007 <satar.noory@hyperisland.se>
2025-01-09 18:52:34 +0100

    Assignment 6

---
+++ 6/script.js
@@ -0,0 +1,49 @@
+// TODO: Set item in localStorage
+
+localStorage.setItem("key", "value");
+
+// TODO: Get item from localStorage
+
+localStorage.getItem("key");
+
+// TODO: Remove item from localStorage
+
+localStorage.removeItem("key");
+
+// TODO: Clear all items from localStorage
+
+localStorage.clear();
+
+// TODO: Add an object to localStorage
+
+let user = { name: "John Doe", age: 30 };
+localStorage.setItem("user", JSON.stringify(user));
+
+// TODO: Get an object from localStorage
+
+JSON.parse(localStorage.getItem("user"));
+
+// TODO: Set item in sessionStorage
+
+sessionStorage.setItem("key", "value");
+
+// TODO: Get item from sessionStorage
+
+sessionStorage.getItem("key");
+
+// TODO: Remove item from sessionStorage
+
+sessionStorage.removeItem("key");
+
+// TODO: Clear all items from sessionStorage
+
+sessionStorage.clear();
+
+// TODO: Add an object to sessionStorage
+
+let user = { name: "John Doe", age: 30 };
+sessionStorage.setItem("user", JSON.stringify(user));
+
+// TODO: Get an object from sessionStorage
+
+JSON.parse(sessionStorage.getItem("user"));
