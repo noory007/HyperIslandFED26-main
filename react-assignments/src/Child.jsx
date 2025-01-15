10b10a968012854daabb086a0b9b5c2dbe5bb710
noory007 <satar.noory@hyperisland.se>
2025-01-15 20:11:22 +0100

    React Assignment - 8

---
+++ react-assignments/src/Child.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+export function Child({ person }) {
+  console.log(person);
+  return (
+    <div>
+      <h2>Name: {person.name}</h2>
+      <h2>Age: {person.info.age} </h2>
+      <h2>City: {person.info.city} </h2>
+      <h2>Job: {person.info.job} </h2>
+      <h2>Hobbies: {person.info.hobbies + ""} </h2>
+    </div>
+  );
+}
+export default Child;
